2f1fbddafc4e5574f107ff5f8e4426f417b09f12
Katy <dreadpiratekaty@gmail.com>
2018-03-16 13:37:16 -0700

    Pixel tutorials

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"image"
+	_ "image/png"
+	"os"
+
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+)
+
+func main() {
+	pixelgl.Run(run)
+}
+
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "You Better Work",
+		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+	pic, err := loadPicture("hikingGopher.png")
+	if err != nil {
+		panic(err)
+	}
+
+	sprite := pixel.NewSprite(pic, pic.Bounds())
+
+	win.Clear(colornames.Skyblue)
+	sprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
+
+	for !win.Closed() {
+		win.Update()
+	}
+}
+
+func loadPicture(path string) (pixel.Picture, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	return pixel.PictureDataFromImage(img), nil
+}
